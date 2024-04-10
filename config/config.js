ff6ce4bd010045c78b6338b5bd9b162eb572637b
rankr6 <rushikesh.kakadiya114652@marwadiuniversity.ac.in>
2024-04-11 02:09:06 +0530

    env changes

--- config/config.js
+++ config/config.js
@@ -3,10 +3,9 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DEV_USER,
+    username: process.env.DEV_USERNAME,
     password: process.env.DEV_PASSWORD,
     database: process.env.DEV_DATABASE,
-    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 
