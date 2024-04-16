ed177b67968bbbe0655e0166f7241c154b1b6dc5
rankr6 <rushikesh.kakadiya114652@marwadiuniversity.ac.in>
2024-04-16 15:49:32 +0530

    changes

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
